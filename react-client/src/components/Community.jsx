374e115bd68cec06169d644dc33cfd316b8b55a7
pnn2104@columbia.edu <pnn2104@columbia.edu>
2018-03-07 09:27:16 -0500

    Adding listings & modal slideshow...still need to work on styling

--- react-client/src/components/Community.jsx
+++ react-client/src/components/Community.jsx
@@ -1,14 +1,49 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import postDummyData from './postDummyData.jsx';
+import Listing from './Listing.jsx'
+import ListingModal from './ListingModal.jsx';
 
 export default class CommunityBoard extends Component {
 	constructor(props){
 		super(props)
 		this.state = {
-
+			listings: postDummyData,
+			open: false,
+			slidesMedia: []
+			//options: ['Bedroom', 'Kitchen', 'Dining Room', 'Appliance', 'Electronics', 'Clothes', 'Misc']
+			//showManageListings: false
 		}
+		this.handleClose = this.handleClose.bind(this);
+		this.openSlideShow = this.openSlideShow.bind(this);
 	}
 
-	render(){
-		return (<h1>Community</h1> )
+	openSlideShow(target) {
+		this.setState({
+			open: !this.state.open,
+			slidesMedia: target.image
+		});
+	}
+
+	handleClose() {
+		this.setState({
+			open: false
+		});
+	}
+
+	render() {
+		//rendering the modal that displays the listings' pictures
+		if (this.state.open) {
+			return <ListingModal slidesMedia={this.state.slidesMedia} open={this.state.open} handleClose={this.handleClose}/>
+		} 
+		return (
+			<div className="community-board">
+				
+				{this.state.listings.map((listing, i) => {
+					return (
+						<Listing key={i} listing={listing} openSlideShow={this.openSlideShow}/>
+					)
+				})}
+			</div>
+		)
 	}
 }
